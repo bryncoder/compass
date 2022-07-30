62ff5e4730913f4c7802c77ae57ea125c9a2e34e
bryncoder <110289526+bryncoder@users.noreply.github.com>
2022-07-30 12:41:25 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.calibrateCompass()
+input.isGesture(Gesture.Shake), function () {
+    basic.showString("" + (input.compassHeading()))
+    if (input.compassHeading() > 337.6 && input.compassHeading() < 360) {
+        basic.showArrow(ArrowNames.North)
+    } else if (input.compassHeading() >= 0 && input.compassHeading() <= 22.5) {
+        basic.showArrow(ArrowNames.North)
+    } else if (input.compassHeading() >= 22.6 && input.compassHeading() <= 67.5) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (input.compassHeading() >= 67.6 && input.compassHeading() <= 112.5) {
+        basic.showArrow(ArrowNames.East)
+    } else if (input.compassHeading() >= 112.6 && input.compassHeading() <= 157.5) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (input.compassHeading() >= 175.6 && input.compassHeading() <= 202.5) {
+        basic.showArrow(ArrowNames.South)
+    } else if (input.compassHeading() >= 202.6 && input.compassHeading() <= 247.5) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (input.compassHeading() >= 247.6 && input.compassHeading() <= 292.5) {
+        basic.showArrow(ArrowNames.West)
+    } else if (input.compassHeading() >= 292.6 && input.compassHeading() <= 337.5) {
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+}
